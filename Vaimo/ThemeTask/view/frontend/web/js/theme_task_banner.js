ae7627f58ad84d1f63fd64c0aee9e8e607017964
Qiang <qiang.zhang@vaimo.com>
2022-04-19 11:33:00 +0200

    optize banner with jquery

--- Vaimo/ThemeTask/view/frontend/web/js/theme_task_banner.js
+++ Vaimo/ThemeTask/view/frontend/web/js/theme_task_banner.js
@@ -1,84 +1,57 @@
-define([], function(){
-    var oBody = document.getElementsByTagName("body")[0];
-    var aBanner = document.getElementsByClassName("banner");
-    var aSpan = document.getElementsByClassName("tab")[0].getElementsByTagName("span");
-    var oNext = document.getElementsByClassName("next")[0];
-    var Oprev = document.getElementsByClassName("prev")[0];
-    var Oon = document.getElementsByClassName("on")[0];
+define([
+    'jquery'
+], function($){
 
-    aBanner[0].style.opacity = "1";
-    aSpan[0].className = "on";
+    var aSpan = $(".tab").find('span');
+    var currIndex = 0;
+    show(currIndex,-1);
 
-    var num = 0;
-    for(var i = 0;i < aSpan.length;i++) {
-        aSpan[i].index = i;
-        aSpan[i].onclick = function(){
-            //click point to show specific picture
-            for(var j = 0 ;j < aSpan.length; j++){
-                num = this.index;
-                aSpan[j].className = "";
-                aBanner[j].style.opacity = "0";
-            }
-            aSpan[num].className = "on";
-            aBanner[num].style.opacity = "1";
+    function show(nidx, oidx) {
+        if( oidx >= 0 ) {
+            $(".banner").eq(oidx).css({opacity:0});
+            $(".tab > span").eq(oidx).removeClass('on');
         }
-
-        oNext.onclick = function() {
-            //trigger event to show next
-            for(var j = 0 ;j < aSpan.length; j++){
-                if(aSpan[j].className == "on"){
-                    aSpan[j].className = "";
-                    aBanner[j].style.opacity = "0";
-                    j++;
-                    num++;
-                    if(j > 4){
-                        j = 0;
-                    }
-                    aSpan[j].className = "on";
-                    aBanner[j].style.opacity = "1";
-                }
-            }
+        if( nidx >= aSpan.length) {
+            nidx = 0;
+            currIndex = -1;
         }
-
-        Oprev.onclick = function() {
-            //trigger event to show last
-            for(var j = 0 ;j < aSpan.length; j++){
-                if(aSpan[j].className == "on"){
-                    aSpan[j].className = "";
-                    aBanner[j].style.opacity = "0";
-                    j--;
-                    num--;
-                    if(j < 0){
-                        j = 4;
-                    }
-                    aSpan[j].className = "on";
-                    aBanner[j].style.opacity = "1";
-                }
-            }
+        if(nidx < 0) {
+            nidx = aSpan.length - 1;
+            currIndex = aSpan.length;
         }
+        $(".banner").eq(nidx).css({opacity:1});
+        $(".tab > span").eq(nidx).addClass('on');
     }
-    /*function for timer */
-    function startTimer() {
-        num++;
-        if(num < 5){
-            for(var j = 0 ;j < aSpan.length; j++){
-                aSpan[j].className = "";
-                aBanner[j].style.opacity = "0";
-            }
-            aSpan[num].className = "on";
-            aBanner[num].style.opacity = "1";
-        }else {
-            num = -1;
-        }
-    }
-    clearInterval(timer);
-    var timer = setInterval("startTimer",2000);/*call timer*/
 
-    oBody.onmouseover = function(){/*mouse movein，clear timer，stop rotate*/
+    $(".next").click(function(){
+        show(currIndex+1, currIndex)
+        currIndex ++;
+    });
+
+    $(".prev").click(function(){
+        show(currIndex-1, currIndex)
+        currIndex --;
+    });
+
+    $(".tag > span").each(function(i) {
+        $(this).on('click', function(i) {
+            show(i, currIndex);
+            currIndex = i;
+        });
+    });
+
+    function auto() {
+        show(currIndex+1, currIndex);
+        currIndex ++;
+    }
+    // clearInterval(timer);
+    var timer = setInterval(function(){ auto(); },2000);
+    $('#theme_task_bander_wrap').mouseover(function (){
         clearInterval(timer);
-    };
-    oBody.onmouseout = function(){/*mouse moveout，recall timer*/
+    });
+    $('#theme_task_bander_wrap').mouseout(function (){
         clearInterval(timer);
-        timer = setInterval("startTimer",2000);
-    };
+        timer = setInterval(function(){ auto();},2000);
+    });
+
 });
